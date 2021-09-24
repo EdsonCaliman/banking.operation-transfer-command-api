e2bc046a9c3d4e9233f34586ed83abbb3a614f0b
Edson Caliman <edsonjunior@db1.com.br>
2021-09-24 16:42:30 -0300

    Defined attributes for RequestTransferDto

--- Banking.Operation.Transfer.Command.Domain/Transfer/Dtos/RequestTransferDto.cs
+++ Banking.Operation.Transfer.Command.Domain/Transfer/Dtos/RequestTransferDto.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Banking.Operation.Transfer.Command.Domain.Transfer.Dtos
 {
     public class RequestTransferDto
     {
+        [Required(ErrorMessage = "ContactId is mandatory")]
         public Guid ContactId { get; set; }
+        [Required(ErrorMessage = "Value is mandatory")]
+        [Range(0, 10000, ErrorMessage = "Value must be between 0 and 10000")]
         public decimal Value { get; set; }
     }
 }
